daf4a4d30650d7f913a42c251449abef5e687214
JQ <jorgequintt@gmail.com>
2026-06-20 17:38:56 -0500

    fix: remove broken icon on refImage empty

--- src/workspace/Workspace.ts
+++ src/workspace/Workspace.ts
@@ -166,6 +166,7 @@ export async function Workspace(config: WorkspaceInputConfig) {
 
   // MARK: Elements
   const refImage = document.createElement('img');
+  refImage.alt = ""; // removes broken icon
   const refImageWrapper = document.createElement('div');
   refImageWrapper.append(refImage);
   refImageWrapper.style = `position: absolute; top: 0px; right: 0px; z-index: -999; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;`;
