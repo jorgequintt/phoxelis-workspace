7048e18a8e92785a9e4c812d69c43bc2df95589e
JQ <jorgequintt@gmail.com>
2026-06-25 15:31:43 -0500

    refactor: remove unused line

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import reactPlugin from '@vitejs/plugin-react-swc';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [
